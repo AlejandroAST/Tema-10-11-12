191b7678eb9422d26296b9c0a652769a8111c74b
Alejandro Fernández Fernández <111458168+AlejandroAST@users.noreply.github.com>
2022-11-26 09:22:12 +0100

    Commit Inicial Tema-10-11-12.v.1.0

---
+++ src/main/java/impl/Tema101112/Tema101112Application.java
@@ -0,0 +1,13 @@
+package impl.Tema101112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tema101112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tema101112Application.class, args);
+	}
+
+}
---
+++ src/test/java/impl/Tema101112/Tema101112ApplicationTests.java
@@ -0,0 +1,13 @@
+package impl.Tema101112;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tema101112ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
