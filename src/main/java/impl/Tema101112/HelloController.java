111b812a46b7079ea164fb1e14f8a3cc464db799
Alejandro Fernández Fernández <111458168+AlejandroAST@users.noreply.github.com>
2022-11-26 09:29:50 +0100

    Añadido HelloController y ficheros de propiedades

---
+++ src/main/java/impl/Tema101112/HelloController.java
@@ -0,0 +1,18 @@
+package impl.Tema101112;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")
+    String mensaje;
+
+    @GetMapping("/holaMundo")
+    public String holaMundo() {
+        System.out.println(mensaje);
+        return "Hola Mundo";
+    }
+}
--- src/main/java/impl/Tema101112/Tema101112Application.java
+++ src/main/java/impl/Tema101112/Tema101112Application.java
@@ -8,6 +8,12 @@ public class Tema101112Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Tema101112Application.class, args);
+		System.out.print("""
+                <>-------<> TEMA 10-11-12 <>------<>
+                +-------------------------------+
+                |  Ejercicios 1-2-3 Spring      |
+                +-------------------------------+
+                """);
 	}
 
 }
